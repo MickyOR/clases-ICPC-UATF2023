d13950b9fdd333413c963ca0891aa6879c411816
Miguel Ortiz <mickyor97@gmail.com>
2023-10-09 23:04:37 -0400

    Soluciones busqueda binaria y teoria de numeros

---
+++ CodigoAuxiliar/ContestBusquedaBinaria/A_Sum_of_Three_Values.cpp
@@ -0,0 +1,86 @@
+/*
+               __
+              / _)
+     _.----._/ /
+    /         /
+ __/ (  | (  |
+/__.-'|_|--|_|
+*/
+#include <bits/stdc++.h>
+ //#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include <bits/extc++.h>
+typedef long long ll;
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define all(a) (a).begin(), (a).end()
+#define clr(a, h) memset(a, (h), sizeof(a))
+#define f first
+#define s second
+#define fore(i, b, e) for (int i = (int) b, o_o = e; i < (int) o_o; ++i)
+#define forr(i, b, e) for (int i = (int) b, o_o = e; i < (int) o_o; ++i)
+#define deb(x) cerr << "# " << (#x) << " = " << (x) << endl;
+#define sz(x) (int) x.size()
+#define endl '\n'
+// int in(){int r=0,c;for(c=getchar();c<=32;c=getchar());if(c=='-') return -in();for(;c>32;r=(r<<1)+(r<<3)+c-'0',c=getchar());return r;}
+
+using namespace std;
+#define cin std::cin
+#define cout std::cout
+#define cerr std::cerr
+//using namespace __gnu_pbds;
+//#pragma GCC target ("avx2")
+//#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+
+typedef pair < int, int > ii;
+typedef vector < int > vi;
+typedef vector < ii > vii;
+typedef vector < ll > vll;
+typedef vector < vi > vvi;
+typedef vector < vii > vvii;
+//typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+//find_by_order kth largest  order_of_key <
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int INF = numeric_limits < int > ::max() / 4;
+const double PI = acos(-1);
+
+signed main() {
+  std::ios::sync_with_stdio(false); cin.tie(0);
+  int n, x;
+  cin >> n >> x;
+  vector<pair<int, int>> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i].first;
+    a[i].second = i + 1;
+  }
+  sort(a.begin(), a.end());
+  for (int i = 0; i < n; i++) {
+    for (int j = i+1; j < n; j++) {
+      int sum = a[i].first + a[j].first;
+      int need = x - sum;
+      int lo = j + 1, hi = n - 1;
+      int res = -1;
+      while (lo <= hi) {
+        int mid = (lo + hi) / 2;
+        if (a[mid].first == need) {
+          res = mid;
+          break;
+        } else if (a[mid].first < need) {
+          lo = mid + 1;
+        } else {
+          hi = mid - 1;
+        }
+      }
+      if (res != -1) {
+        cout << a[i].second << " " << a[j].second << " " << a[res].second << endl;
+        return 0;
+      }
+    }
+  }
+  cout << "IMPOSSIBLE" << endl;
+  return 0;
+}
+// Sunrise, Parabellum
